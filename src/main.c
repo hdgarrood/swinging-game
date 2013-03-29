15066c30f08c93fede2f3832106faf6af3f9a120
Harry Garrood <h@hdgarrood.me.uk>
2013-03-29 19:13:56 +0000

    add debug_print()

--- src/main.c
+++ src/main.c
@@ -9,6 +9,16 @@
 #include "DebugDraw.h"
 #include "Timer.h"
 
+#define DEBUG 1
+
+#define debug_println(fmt, ...) \
+        do { if (DEBUG) fprintf(stderr, "%s:%d in %s(): " fmt "\n", __FILE__, \
+                                __LINE__, __func__, __VA_ARGS__); } while (0)
+
+#define debug_print(fmt, ...) \
+        do { if (DEBUG) fprintf(stderr, "%s:%d in %s(): " fmt, __FILE__, \
+                                __LINE__, __func__, __VA_ARGS__); } while (0)
+
 const int SCREEN_WIDTH  = 640;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP    = 32;

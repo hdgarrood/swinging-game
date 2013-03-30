519508d61938d383ef26b579e8a080ed63f1a104
Harry Garrood <h@hdgarrood.me.uk>
2013-03-30 16:20:02 +0000

    print to stdout so that output can be grepped

--- src/macros.h
+++ src/macros.h
@@ -1,9 +1,9 @@
 #define DEBUG 1
 
 #define debug_putsf(fmt, ...) \
-        do { if (DEBUG) fprintf(stderr, "%s:%d in %s(): " fmt "\n", __FILE__, \
+        do { if (DEBUG) fprintf(stdout, "%s:%d in %s(): " fmt "\n", __FILE__, \
                                 __LINE__, __func__, __VA_ARGS__); } while (0)
 
 #define debug_puts(msg) \
-        do { if (DEBUG) fprintf(stderr, "%s:%d in %s(): " msg "\n", \
+        do { if (DEBUG) fprintf(stdout, "%s:%d in %s(): " msg "\n", \
                                 __FILE__, __LINE__, __func__); } while (0)

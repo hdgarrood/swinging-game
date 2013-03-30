3d47816c7ca00a2d13fc36ede3ff7ba40bb2537c
Harry Garrood <h@hdgarrood.me.uk>
2013-03-30 02:09:10 +0000

    remove unnecessary sigint handler

--- src/main.c
+++ src/main.c
@@ -34,31 +34,8 @@
 /*     SDL_BlitSurface(source, NULL, dest, &offset); */
 /* } */
 
-
-void
-catch_signal_and_die(int signal)
-{
-    printf("Caught signal %d\n", signal);
-    exit(1);
-}
-
-/*
- * use sigaction to register a SIGINT handler, which causes the program to
- * exit.
- */
-void
-setup_signals()
-{
-    struct sigaction sigint_handler;
-    sigint_handler.sa_handler = catch_signal_and_die;
-    sigemptyset(&sigint_handler.sa_mask);
-    sigint_handler.sa_flags = 0;
-    sigaction(SIGINT, &sigint_handler, NULL);
-}
-
 int main()
 {
-    setup_signals();
     SDL_Init(SDL_INIT_EVERYTHING);
 
     struct game *game = make_game();

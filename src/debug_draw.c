c34e2a997943900e97f8885aecd8c78f9eaee043
Harry Garrood <h@hdgarrood.me.uk>
2013-04-02 01:55:51 +0100

    fix whitespace

--- src/debug_draw.c
+++ src/debug_draw.c
@@ -58,45 +58,45 @@ draw_segment_shape(struct draw_options opts, cpShape* shape, cpBody* body)
 static void
 draw_poly_shape(struct draw_options opts, cpShape *shape, cpBody *body)
 {
-	int num_verts = cpPolyShapeGetNumVerts(shape);
-	cpVect first_vert = cpBodyLocal2World(body, cpPolyShapeGetVert(shape, 0));
-	cpVect prev_vert = first_vert;
-	cpVect pos = cpBodyGetPos(body);
+    int num_verts = cpPolyShapeGetNumVerts(shape);
+    cpVect first_vert = cpBodyLocal2World(body, cpPolyShapeGetVert(shape, 0));
+    cpVect prev_vert = first_vert;
+    cpVect pos = cpBodyGetPos(body);
 
-	for (int i = 1; i < num_verts; i++)
-	{
-		cpVect vert = cpBodyLocal2World(body, cpPolyShapeGetVert(shape, i));
-		draw_line(opts, prev_vert, vert);
-		prev_vert = vert;
-	}
+    for (int i = 1; i < num_verts; i++)
+    {
+        cpVect vert = cpBodyLocal2World(body, cpPolyShapeGetVert(shape, i));
+        draw_line(opts, prev_vert, vert);
+        prev_vert = vert;
+    }
 
-	/* close up the polygon */
-	draw_line(opts, prev_vert, first_vert);
+    /* close up the polygon */
+    draw_line(opts, prev_vert, first_vert);
 }
 
 static void
 draw_shape(cpShape* shape, SDL_Surface* screen)
 {
-	cpBody* body = shape->body;
+    cpBody* body = shape->body;
     struct draw_options opts = {
         .surface = screen,
         .colour = colour(200, 200, 200)
     };
 
-	switch (shape->CP_PRIVATE(klass)->type)
-	{
-		case CP_CIRCLE_SHAPE:
+    switch (shape->CP_PRIVATE(klass)->type)
+    {
+        case CP_CIRCLE_SHAPE:
             draw_circle_shape(opts, shape, body);
             break;
         case CP_SEGMENT_SHAPE:
             draw_segment_shape(opts, shape, body);
             break;
-		case CP_POLY_SHAPE:
-			draw_poly_shape(opts, shape, body);
-			break;
+        case CP_POLY_SHAPE:
+            draw_poly_shape(opts, shape, body);
+            break;
         default:
-			debug_putsf("ignoring unrecognised shape type %d",
-					shape->CP_PRIVATE(klass)->type);
+            debug_putsf("ignoring unrecognised shape type %d",
+                    shape->CP_PRIVATE(klass)->type);
             break;
     }
 }
@@ -109,9 +109,9 @@ draw_constraint(cpConstraint* constraint, SDL_Surface *screen)
         .colour = colour(100, 100, 200)
     };
 
-	cpVect vect_a = cpBodyGetPos(cpConstraintGetA(constraint));
-	cpVect vect_b = cpBodyGetPos(cpConstraintGetB(constraint));
-	draw_line(opts, vect_a, vect_b);
+    cpVect vect_a = cpBodyGetPos(cpConstraintGetA(constraint));
+    cpVect vect_b = cpBodyGetPos(cpConstraintGetB(constraint));
+    draw_line(opts, vect_a, vect_b);
 }
 
 static void
@@ -152,12 +152,12 @@ draw_body(cpBody *body, SDL_Surface *screen)
 void
 debug_draw_space(cpSpace* space, SDL_Surface* screen)
 {
-	cpSpaceEachShape(space,
-		(cpSpaceShapeIteratorFunc)draw_shape,
-		screen);
-	cpSpaceEachConstraint(space,
-		(cpSpaceConstraintIteratorFunc)draw_constraint,
-		screen);
+    cpSpaceEachShape(space,
+        (cpSpaceShapeIteratorFunc)draw_shape,
+        screen);
+    cpSpaceEachConstraint(space,
+        (cpSpaceConstraintIteratorFunc)draw_constraint,
+        screen);
     cpSpaceEachBody(space,
         (cpSpaceBodyIteratorFunc)draw_body,
         screen);
